d139e21eaf819b1bea0ff8fbe5c6716fadef2f3e
WebForks <35718739+WebForks@users.noreply.github.com>
2025-02-26 10:19:27 -0800

    started switching over nativewind instead of native css

--- Tenuity/app/(tabs)/about.tsx
+++ Tenuity/app/(tabs)/about.tsx
@@ -1,21 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 export default function AboutScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>About scree2</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    color: "#fff",
-  },
-});
