02206006f2fad0e1c4c4841b2d9d064350601fa4
Tyler Alamo-Covert <tyleralamo15@gmail.com>
2025-02-28 09:05:06 -0800

    Add firebase configuration

--- Tenuity/firebaseConfig.js
+++ Tenuity/firebaseConfig.js
@@ -1,24 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
-// import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
-// import {...} from 'firebase/functions';
-// import {...} from 'firebase/storage';
-
-// Initialize Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+  apiKey: "AIzaSyCCxODzkTfqeKQfeL6okRzkBeRq_dDYfvI",
+  authDomain: "tenuity-491.firebaseapp.com",
+  projectId: "tenuity-491",
+  storageBucket: "tenuity-491.firebasestorage.app",
+  messagingSenderId: "350117112199",
+  appId: "1:350117112199:web:a2079d36f42b1f341bcb22",
+  measurementId: "G-P28PRYBMQH"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+const analytics = getAnalytics(app);
