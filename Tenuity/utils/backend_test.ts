2a70a2f7bffe9b385f55349a7014b83d482cc176
Tyler Alamo-Covert <tyleralamo15@gmail.com>
2025-03-07 15:14:13 -0800

    Script to manually add auth users to the database

---
+++ Tenuity/utils/backend_test.ts
@@ -0,0 +1,80 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://tedyjfkbdwhziszwjmgl.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlZHlqZmtiZHdoemlzendqbWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyODE2MzgsImV4cCI6MjA1Njg1NzYzOH0.IuSjZGMdcgYO1yy-n4FBHs7XCW1N9mjTvZEjwmFjTvI";
+
+// Initializing connection to the database
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+// Adding user to the landlord tabl
+const createUser = async(user) => {
+  const {error} = await supabase
+  .from('Landlords')
+  .insert([
+      {
+        id: user.id,
+        email: user.email,
+        password: user.password,
+        first_name: "Jesse",
+        last_name: "Pinkman",
+        phone_number: "123-456-7890"
+      }
+    ])
+
+  if(error) {
+    console.log("Error while adding user to the table:",error);
+  } else {
+    console.log("Successfully created user!:", user.email)
+  }
+}
+
+
+
+// Signing up a test user with hard coded values
+const signUpUser = async() => {
+  
+  const {data,error} = await supabase.auth.signUp({
+    email: "jessepinkman@gmail.com",
+    password: "password",
+    email_confirm: false
+  });
+
+  if(error){
+    console.log("Error:", error);
+  } else{
+    console.log("Successfully signed up user!");
+  }
+
+  // Manually confirm user using Admin API
+  if (data.user) {
+    const { error: confirmError } = await supabase.auth.admin.updateUserById(
+      data.user.id, // User ID
+      { email_confirmed_at: new Date().toISOString() } // Confirm email
+    );
+
+    if (confirmError) {
+      console.error("Error confirming user:", confirmError.message);
+      return;
+    }
+
+    console.log("User successfully confirmed!");
+  }
+
+
+
+  const user = data.user;
+
+  if(user) {
+    await createUser(user);
+  }
+}
+
+// Calling function to complete manually insertion to the database
+signUpUser();
