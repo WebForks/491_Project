0d48d915a7015de994b673bcd03d5288f1c993d7
Tyler <tyleralamo15@gmail.com>
2025-03-09 19:42:41 -0700

    Adding pre-authenticated users to Landlords table

--- Tenuity/utils/backend_test.ts
+++ Tenuity/utils/backend_test.ts
@@ -13,43 +13,50 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   },
 });
 
-// Adding user to the landlord tabl
-const createUser = async (user) => {
-  const { error } = await supabase.from("Landlords").insert([
-    {
-      id: user.id,
-      email: user.email,
-      password: user.password,
-      first_name: "Jesse",
-      last_name: "Pinkman",
-      phone_number: "123-456-7890",
-    },
-  ]);
+// Adding user to the landlord table
+const createUser = async (userEmail, userId) => {
+  const { data, error } = await supabase.from("Landlords").insert({
+    //id: userId,
+    email: userEmail,
+    first_name: "Jesse",
+    last_name: "Pinkman",
+    phone_number: "123-456-7890",
+  });
 
   if (error) {
     console.log("Error while adding user to the table:", error);
+    console.log(data);
   } else {
-    console.log("Successfully created user!:", user.email);
+    console.log("Successfully created user!:", userEmail);
   }
 };
 
 // Signing up a test user with hard coded values
 const signUpUser = async () => {
-  const { data, error } = await supabase.auth.signUp({
-    email: "jessepinkman@gmail.com",
+  //const { data, error } = await supabase.auth.signUp({
+  //email: "joe@gmail.com",
+  //password: "password",
+  //});
+
+  //if (error) {
+  //console.log("Error:", error);
+  //} else {
+  // console.log("Successfully signed up user!");
+  //}
+  //
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: "joe@gmail.com",
     password: "password",
-    email_confirm: false,
   });
 
-  if (error) {
-    console.log("Error:", error);
-  } else {
-    console.log("Successfully signed up user!");
-  }
-  const user = data.user;
+  //const { data, error } = await supabase.auth.getUser();
+  const userEmail = data.user.email;
+  const userId = data.user.id;
+  console.log(userEmail);
+  console.log(userId);
 
-  if (user) {
-    await createUser(user);
+  if (data) {
+    await createUser(userEmail, userId);
   }
 };
 
