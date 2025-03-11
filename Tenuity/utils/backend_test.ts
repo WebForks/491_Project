ca4fab68a026db8fa70bcc4b25ebb703328548b7
Tyler <tyleralamo15@gmail.com>
2025-03-10 23:14:56 -0700

    Changing documentation

--- Tenuity/utils/backend_test.ts
+++ Tenuity/utils/backend_test.ts
@@ -15,9 +15,7 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 
 // Adding user to the landlord table
 const createUser = async (userEmail, userId) => {
-<<<<<<< HEAD
   const { data, error } = await supabase.from("Landlords").insert({
-=======
     id: userId,
     email: userEmail,
     first_name: "Tyler",
@@ -34,7 +32,6 @@ const createUser = async (userEmail, userId) => {
 
 // Signing up a test user with hard coded values
 const signUpUser = async () => {
-<<<<<<< HEAD
   //const { data, error } = await supabase.auth.signUp({
   //email: "tyleralamo20@gmail.com",
   //password: "password",
@@ -46,46 +43,39 @@ const signUpUser = async () => {
   //console.log("Successfully signed up user!");
   //}
 
-=======
   const { data, error } = await supabase.auth.signInWithPassword({
     email: "tyleralamo20@gmail.com",
     password: "password",
   });
- 
-  if (error){
+
+  if (error) {
     console.log("Error:", error);
-  } else{
+  } else {
     //const { data, error } = await supabase.auth.getUser();
     const userEmail = data.user.email;
     const userId = data.user.id;
     console.log(userEmail);
     console.log(userId);
 
-<<<<<<< HEAD
-=======
     if (data) {
       await createUser(userEmail, userId);
     }
   }
 };
->>>>>>> 3ebf2bad43d0ba365cc549febff719f32ea58c58
 
-
-// Adding random data into a blank table 
+// Adding random data into a blank table
 // This will be the template for how data is added into the database
 // Make sure to disbale RLS for adding to the table -- temp
-const addToDatabase = async() => {
-  const {error} = await supabase
-    .from("Test")
-    .insert({
-      name: "Just Testing", 
-      income: "$80,000"
-    })
-  
-  if(error){
+const addToDatabase = async () => {
+  const { error } = await supabase.from("Test").insert({
+    name: "Just Testing",
+    income: "$80,000",
+  });
+
+  if (error) {
     console.log("Error while inserting to test table:", error);
   }
 };
 
 // Calling function to complete manually insertion to the database
-addAuthUserToDB();
+//addAuthUserToDB();
