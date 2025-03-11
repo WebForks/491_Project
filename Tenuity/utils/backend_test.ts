74b78bf6526b87269b54826a03ff5dd60d0a6121
Tyler <tyleralamo15@gmail.com>
2025-03-10 23:07:43 -0700

    Changing testing criteria

--- Tenuity/utils/backend_test.ts
+++ Tenuity/utils/backend_test.ts
@@ -16,10 +16,10 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 // Adding user to the landlord table
 const createUser = async (userEmail, userId) => {
   const { data, error } = await supabase.from("Landlords").insert({
-    //id: userId,
+    id: userId,
     email: userEmail,
-    first_name: "Jesse",
-    last_name: "Pinkman",
+    first_name: "Tyler",
+    last_name: "Testing",
     phone_number: "123-456-7890",
   });
 
@@ -34,22 +34,27 @@ const createUser = async (userEmail, userId) => {
 // Signing up a test user with hard coded values
 const signUpUser = async () => {
   //const { data, error } = await supabase.auth.signUp({
-  //email: "joe@gmail.com",
+  //email: "tyleralamo20@gmail.com",
   //password: "password",
   //});
 
   //if (error) {
   //console.log("Error:", error);
   //} else {
-  // console.log("Successfully signed up user!");
+  //console.log("Successfully signed up user!");
   //}
-  //
+
   const { data, error } = await supabase.auth.signInWithPassword({
-    email: "joe@gmail.com",
+    email: "tyleralamo20@gmail.com",
     password: "password",
   });
 
+  if (error) {
+    console.log("Error signing in:", error);
+  }
+
   //const { data, error } = await supabase.auth.getUser();
+  console.log(data);
   const userEmail = data.user.email;
   const userId = data.user.id;
   console.log(userEmail);
