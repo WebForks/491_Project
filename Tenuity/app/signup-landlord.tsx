cb2c865fae2ead01102d057611fa00119e3a1e8e
Evgin Demirci <128102917+Evolancer02@users.noreply.github.com>
2025-03-12 10:42:37 -0700

    Added the option to be able to scroll for the signup page as well as added TO DOs.

--- Tenuity/app/signup-landlord.tsx
+++ Tenuity/app/signup-landlord.tsx
@@ -1,3 +1,7 @@
+/* TO DOS:
+  - Make the footer white like the rest of the page
+  - Figure out how to prevent the keyboard from covering the input fields
+*/
 import React, { useState } from "react";
 import {
   Alert,
@@ -7,6 +11,7 @@ import {
   TouchableOpacity,
   SafeAreaView,
   Image,
+  ScrollView,
 } from "react-native";
 import "../global.css";
 import { Link } from "expo-router";
@@ -55,101 +60,103 @@ export default function SignupLandlord() {
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-white p-4">
-      {/* Header */}
-      <View className="items-center mb-8">
-        <Image
-          source={require("../assets/images/logo.png")}
-          className="w-[150px] h-[150px]"
-          resizeMode="contain"
-        />
-      </View>
-
-      {/* Signup Form */}
-      <View className="border-2 border-[#38B6FF] rounded-lg p-4 mx-4">
-        <View className="mb-4">
-          <Text className="text-left text-lg mb-1">First Name</Text>
-          <TextInput
-            value={firstName}
-            onChangeText={setFirstName}
-            className="border-2 border-[#38B6FF] rounded-lg p-2"
-            placeholder="Enter your name"
-            placeholderTextColor="#888"
+    <ScrollView>
+      <SafeAreaView className="flex-1 bg-white p-4">
+        {/* Header */}
+        <View className="items-center mb-8">
+          <Image
+            source={require("../assets/images/logo.png")}
+            className="w-[150px] h-[150px]"
+            resizeMode="contain"
           />
         </View>
 
-        <View className="mb-4">
-          <Text className="text-left text-lg mb-1">Last Name</Text>
-          <TextInput
-            value={lastName}
-            onChangeText={setLastName}
-            className="border-2 border-[#38B6FF] rounded-lg p-2"
-            placeholder="Enter your name"
-            placeholderTextColor="#888"
-          />
-        </View>
+        {/* Signup Form */}
+        <View className="border-2 border-[#38B6FF] rounded-lg p-4 mx-4">
+          <View className="mb-4">
+            <Text className="text-left text-lg mb-1">First Name</Text>
+            <TextInput
+              value={firstName}
+              onChangeText={setFirstName}
+              className="border-2 border-[#38B6FF] rounded-lg p-2"
+              placeholder="Enter your name"
+              placeholderTextColor="#888"
+            />
+          </View>
 
-        <View className="mb-4">
-          <Text className="text-left text-lg mb-1">Email</Text>
-          <TextInput
-            value={email}
-            onChangeText={setEmail}
-            className="border-2 border-[#38B6FF] rounded-lg p-2"
-            placeholder="Enter your email"
-            keyboardType="email-address"
-            placeholderTextColor="#888"
-          />
-        </View>
+          <View className="mb-4">
+            <Text className="text-left text-lg mb-1">Last Name</Text>
+            <TextInput
+              value={lastName}
+              onChangeText={setLastName}
+              className="border-2 border-[#38B6FF] rounded-lg p-2"
+              placeholder="Enter your name"
+              placeholderTextColor="#888"
+            />
+          </View>
 
-        <View className="mb-4">
-          <Text className="text-left text-lg mb-1">Phone Number</Text>
-          <TextInput
-            value={phoneNumber}
-            onChangeText={setPhoneNumber}
-            className="border-2 border-[#38B6FF] rounded-lg p-2"
-            placeholder="Enter your phone number"
-            keyboardType="phone-pad"
-            placeholderTextColor="#888"
-          />
-        </View>
+          <View className="mb-4">
+            <Text className="text-left text-lg mb-1">Email</Text>
+            <TextInput
+              value={email}
+              onChangeText={setEmail}
+              className="border-2 border-[#38B6FF] rounded-lg p-2"
+              placeholder="Enter your email"
+              keyboardType="email-address"
+              placeholderTextColor="#888"
+            />
+          </View>
 
-        <View className="mb-4">
-          <Text className="text-left text-lg mb-1">Password</Text>
-          <Text className="text-left text-sm mb-1">
-            8 characters minimum, uppercase letter, lowercase letter, and
-            special character (e.g., !, @, #, $)
-          </Text>
-          <TextInput
-            value={password}
-            onChangeText={setPassword}
-            className="border-2 border-[#38B6FF] rounded-lg p-2"
-            placeholder="Enter your password"
-            secureTextEntry
-            placeholderTextColor="#888"
-          />
-        </View>
+          <View className="mb-4">
+            <Text className="text-left text-lg mb-1">Phone Number</Text>
+            <TextInput
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+              className="border-2 border-[#38B6FF] rounded-lg p-2"
+              placeholder="Enter your phone number"
+              keyboardType="phone-pad"
+              placeholderTextColor="#888"
+            />
+          </View>
 
-        <View className="mb-4">
-          <Text className="text-left text-lg mb-1">Confirm Password</Text>
-          <TextInput
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-            className="border-2 border-[#38B6FF] rounded-lg p-2"
-            placeholder="Confirm your password"
-            secureTextEntry
-            placeholderTextColor="#888"
-          />
+          <View className="mb-4">
+            <Text className="text-left text-lg mb-1">Password</Text>
+            <Text className="text-left text-sm mb-1">
+              8 characters minimum, uppercase letter, lowercase letter, and
+              special character (e.g., !, @, #, $)
+            </Text>
+            <TextInput
+              value={password}
+              onChangeText={setPassword}
+              className="border-2 border-[#38B6FF] rounded-lg p-2"
+              placeholder="Enter your password"
+              secureTextEntry
+              placeholderTextColor="#888"
+            />
+          </View>
+
+          <View className="mb-4">
+            <Text className="text-left text-lg mb-1">Confirm Password</Text>
+            <TextInput
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              className="border-2 border-[#38B6FF] rounded-lg p-2"
+              placeholder="Confirm your password"
+              secureTextEntry
+              placeholderTextColor="#888"
+            />
+          </View>
+          <Link href="" asChild>
+            <TouchableOpacity
+              onPress={handleRegister}
+              className="bg-[#38B6FF] w-full py-4 rounded-2xl items-center mb-4"
+            >
+              <Text className="text-white font-bold text-lg">Register</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
-        <Link href="" asChild>
-          <TouchableOpacity
-            onPress={handleRegister}
-            className="bg-[#38B6FF] w-full py-4 rounded-2xl items-center mb-4"
-          >
-            <Text className="text-white font-bold text-lg">Register</Text>
-          </TouchableOpacity>
-        </Link>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
