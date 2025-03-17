6bd416b5b3200cdd6ed0ed5cc1b263f32882d483
Tyler <tyleralamo15@gmail.com>
2025-03-17 10:18:36 -0700

    Fixed broken closing tag

--- Tenuity/app/landlord/signup-landlord.tsx
+++ Tenuity/app/landlord/signup-landlord.tsx
@@ -60,7 +60,6 @@ export default function SignupLandlord() {
   };
 
   return (
-<<<<<<< HEAD:Tenuity/app/landlord/signup-landlord.tsx
     <SafeAreaView className="flex-1 bg-white p-4">
       {/* Header */}
       <View className="items-center mb-8">
@@ -83,92 +82,92 @@ export default function SignupLandlord() {
             placeholderTextColor="#888"
           />
         </View>
+      </View>
 
-        {/* Signup Form */}
-        <View className="border-2 border-[#38B6FF] rounded-lg p-4 mx-4">
-          <View className="mb-4">
-            <Text className="text-left text-lg mb-1">First Name</Text>
-            <TextInput
-              value={firstName}
-              onChangeText={setFirstName}
-              className="border-2 border-[#38B6FF] rounded-lg p-2"
-              placeholder="Enter your name"
-              placeholderTextColor="#888"
-            />
-          </View>
-
-          <View className="mb-4">
-            <Text className="text-left text-lg mb-1">Last Name</Text>
-            <TextInput
-              value={lastName}
-              onChangeText={setLastName}
-              className="border-2 border-[#38B6FF] rounded-lg p-2"
-              placeholder="Enter your name"
-              placeholderTextColor="#888"
-            />
-          </View>
-
-          <View className="mb-4">
-            <Text className="text-left text-lg mb-1">Email</Text>
-            <TextInput
-              value={email}
-              onChangeText={setEmail}
-              className="border-2 border-[#38B6FF] rounded-lg p-2"
-              placeholder="Enter your email"
-              keyboardType="email-address"
-              placeholderTextColor="#888"
-            />
-          </View>
-
-          <View className="mb-4">
-            <Text className="text-left text-lg mb-1">Phone Number</Text>
-            <TextInput
-              value={phoneNumber}
-              onChangeText={setPhoneNumber}
-              className="border-2 border-[#38B6FF] rounded-lg p-2"
-              placeholder="Enter your phone number"
-              keyboardType="phone-pad"
-              placeholderTextColor="#888"
-            />
-          </View>
-
-          <View className="mb-4">
-            <Text className="text-left text-lg mb-1">Password</Text>
-            <Text className="text-left text-sm mb-1">
-              8 characters minimum, uppercase letter, lowercase letter, and
-              special character (e.g., !, @, #, $)
-            </Text>
-            <TextInput
-              value={password}
-              onChangeText={setPassword}
-              className="border-2 border-[#38B6FF] rounded-lg p-2"
-              placeholder="Enter your password"
-              secureTextEntry
-              placeholderTextColor="#888"
-            />
-          </View>
-
-          <View className="mb-4">
-            <Text className="text-left text-lg mb-1">Confirm Password</Text>
-            <TextInput
-              value={confirmPassword}
-              onChangeText={setConfirmPassword}
-              className="border-2 border-[#38B6FF] rounded-lg p-2"
-              placeholder="Confirm your password"
-              secureTextEntry
-              placeholderTextColor="#888"
-            />
-          </View>
-          <Link href="" asChild>
-            <TouchableOpacity
-              onPress={handleRegister}
-              className="bg-[#38B6FF] w-full py-4 rounded-2xl items-center mb-4"
-            >
-              <Text className="text-white font-bold text-lg">Register</Text>
-            </TouchableOpacity>
-          </Link>
+      {/* Signup Form */}
+      <View className="border-2 border-[#38B6FF] rounded-lg p-4 mx-4">
+        <View className="mb-4">
+          <Text className="text-left text-lg mb-1">First Name</Text>
+          <TextInput
+            value={firstName}
+            onChangeText={setFirstName}
+            className="border-2 border-[#38B6FF] rounded-lg p-2"
+            placeholder="Enter your name"
+            placeholderTextColor="#888"
+          />
         </View>
-      </SafeAreaView>
-    </ScrollView>
+
+        <View className="mb-4">
+          <Text className="text-left text-lg mb-1">Last Name</Text>
+          <TextInput
+            value={lastName}
+            onChangeText={setLastName}
+            className="border-2 border-[#38B6FF] rounded-lg p-2"
+            placeholder="Enter your name"
+            placeholderTextColor="#888"
+          />
+        </View>
+
+        <View className="mb-4">
+          <Text className="text-left text-lg mb-1">Email</Text>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            className="border-2 border-[#38B6FF] rounded-lg p-2"
+            placeholder="Enter your email"
+            keyboardType="email-address"
+            placeholderTextColor="#888"
+          />
+        </View>
+
+        <View className="mb-4">
+          <Text className="text-left text-lg mb-1">Phone Number</Text>
+          <TextInput
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+            className="border-2 border-[#38B6FF] rounded-lg p-2"
+            placeholder="Enter your phone number"
+            keyboardType="phone-pad"
+            placeholderTextColor="#888"
+          />
+        </View>
+
+        <View className="mb-4">
+          <Text className="text-left text-lg mb-1">Password</Text>
+          <Text className="text-left text-sm mb-1">
+            8 characters minimum, uppercase letter, lowercase letter, and
+            special character (e.g., !, @, #, $)
+          </Text>
+          <TextInput
+            value={password}
+            onChangeText={setPassword}
+            className="border-2 border-[#38B6FF] rounded-lg p-2"
+            placeholder="Enter your password"
+            secureTextEntry
+            placeholderTextColor="#888"
+          />
+        </View>
+
+        <View className="mb-4">
+          <Text className="text-left text-lg mb-1">Confirm Password</Text>
+          <TextInput
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            className="border-2 border-[#38B6FF] rounded-lg p-2"
+            placeholder="Confirm your password"
+            secureTextEntry
+            placeholderTextColor="#888"
+          />
+        </View>
+        <Link href="" asChild>
+          <TouchableOpacity
+            onPress={handleRegister}
+            className="bg-[#38B6FF] w-full py-4 rounded-2xl items-center mb-4"
+          >
+            <Text className="text-white font-bold text-lg">Register</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </SafeAreaView>
   );
 }
