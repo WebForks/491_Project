464ed0554d8b6791c7748ba0423904f8b7ef12ff
Tyler <tyleralamo15@gmail.com>
2025-03-26 10:06:10 -0700

    Actually fixing the header problem now

--- Tenuity/app/_layout.tsx
+++ Tenuity/app/_layout.tsx
@@ -2,18 +2,10 @@ import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-      <Stack.Screen
-        name="./landlord/dashboard.tsx"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
-      <Stack.Screen name="reset-password" options={{ headerShown: false }} />
-      <Stack.Screen name="profile-landlord" options={{ headerShown: false }} />
-      <Stack.Screen name="signup-landlord" options={{ headerShown: false }} />
-      <Stack.Screen name="change-email" options={{ headerShown: false }} />
-    </Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
   );
 }
