75efbef25195c54dc28b1a7f3ed38a01d8a1c110
Abror Kosimov <abror.ab.ca@gmail.com>
2025-05-06 23:19:30 -0700

    maintenace issue fix

--- Tenuity/app/tenant/tenantMaintenance.tsx
+++ Tenuity/app/tenant/tenantMaintenance.tsx
@@ -185,7 +185,6 @@ export default function Maintenance() {
           address: address,
           cost: 0,
           completed: false,
-          cost: 0, // Auto set cost to 0
           created_at: new Date().toISOString(),
         },
       ]);
